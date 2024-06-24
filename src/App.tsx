387eedc90e731cbb6dadf5a9c6292393ff16ff2e
matveygrevtsov <matveygrevcov@gmail.com>
2024-06-24 04:45:42 +0300

    add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Flex } from "@mantine/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex direction="column" rowGap={16}>
+      <h1>Hello, world!</h1>
+
+      <button>click</button>
+    </Flex>
   );
 }
 
