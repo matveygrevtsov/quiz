0ebc74450eb2cac6697ba8276515eda6081d6448
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2024-06-24 04:46:27 +0300

    add mantine (#1)
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createTheme, MantineProvider } from "@mantine/core";
+
+import "@mantine/core/styles.css";
+import "./index.css";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
 
